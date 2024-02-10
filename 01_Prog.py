ba106d938c0bd42a1d5cd3989b3f0409d81bfe55
jogreyes <jorgereyesgalindo@gmail.com>
2024-02-10 23:03:03 +0200

    Examples_Query

---
+++ 01_Prog.py
@@ -0,0 +1,25 @@
+# DataTypes primitives
+#    float    (float)
+#    Integer  (int)
+#    Boolean  (bool)
+#    String§  (str)
+
+# DataStructure to house DataTypes
+#   my_list  [House any type of data and can mutate]
+#   my_tuple (Same as list but data dont mutate)
+#   my_set   {Can not have the same value twice}
+#   my_dict  {House a keys with one and only one associate value}
+
+
+# MathOperation
+#   a*b multiplication    a**b power
+#   a/b division          a%b  module
+#   a+b addition          a//b absolute division
+#   a-b subtration
+
+# Metods class string
+#     format
+#         example edad = 40
+#                 my_str = f'yo tengo {edad} años'
+#     length
+#         example len(my_str)
