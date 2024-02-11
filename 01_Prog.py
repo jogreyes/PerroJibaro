71a91eb7d4106cb71071943d8a47f9fa4e16a794
jogreyes <jorgereyesgalindo@gmail.com>
2024-02-11 22:56:20 +0200

    Pa la pinga los loops

--- 01_Prog.py
+++ 01_Prog.py
@@ -1,3 +1,5 @@
+from random import randint
+
 # DataTypes primitives
 #    float    (float)
 #    Integer  (int)
@@ -17,9 +19,59 @@
 #   a+b addition          a//b absolute division
 #   a-b subtration
 
+#   from math import ceil, floor, sqrt 
+#   (ceil()) 
+#   (floor())
+#   (sqrt())
+
 # Metods class string
 #     format
 #         example edad = 40
 #                 my_str = f'yo tengo {edad} años'
 #     length
 #         example len(my_str)
+
+# Functions
+#     def funtion_name(parameter):
+#        print(parameter)
+
+#     def pow_2(x):
+#        return x*x
+
+# Conditionals ?
+#   ==
+#   <=
+#   >=
+#   <
+#   >
+#   !=
+#   is not and or in
+
+#    if true:
+#       print("hello world")
+#    elif:
+#       print("brrrrrr")
+#    else:
+#       print("byebye world")
+
+# Loop
+#   count = 0
+#   while count < 5:
+#       print(count)
+#       count += 1
+
+#   numeros = [1, 2, 3]
+#   for i in numeros:
+#       print(i)
+
+my_list = [randint(0,6) for _ in range(7)]
+
+menor_numero = 0
+
+for elemento in my_list:
+    if elemento <= menor_numero:    # selecccional el primer numro de la lista
+        menor_numero = elemento     # compararlo con todos los demas y ver si es menor
+                                    # en casió de serlo asignarlo a una variable
+
+print(my_list)
+print(menor_numero)
